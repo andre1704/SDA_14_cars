8ec32d3075fb13a2b8e979ee6bd47cedec6ca837
andre1704 <andrzejslowinski93@gmail.com>
2017-03-13 19:58:46 +0100

    1.1

---
+++ carsgui/src/main/java/andrzej/slowinski/cars/CarGuiPanel.java
@@ -0,0 +1,135 @@
+package andrzej.slowinski.cars;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by RENT on 2017-03-13.
+ */
+public class CarGuiPanel extends JPanel {
+    private Car editedCar;
+    private CarsManageGui managerWindow;
+
+    private JLabel jLabelBrand;
+    private JLabel jLabelModel;
+    private JLabel jLabelYear;
+    private JLabel jLabelPower;
+    private JTextField jTextFieldBrand;
+    private JTextField jTextFieldModel;
+    private JTextField jTextFieldYear;
+    private JTextField jTextFieldPower;
+
+    private JButton jButtonSave;
+
+    CarGuiPanel(CarsManageGui window) {
+        this.managerWindow=window;
+        setVisible(true);
+        setLayout(null);
+        setBackground(Color.lightGray);
+        jLabelBrand = new JLabel("Brand");
+        jLabelBrand.setLocation(0, 0);
+        jLabelBrand.setSize(120, 20);
+        add(jLabelBrand);
+        jTextFieldBrand = new JTextField();
+        jTextFieldBrand.setLocation(0, 40);
+        jTextFieldBrand.setSize(120, 20);
+        add(jTextFieldBrand);
+        jLabelModel = new JLabel("Model");
+        jLabelModel.setSize(120, 20);
+        jLabelModel.setLocation(0, 80);
+        add(jLabelModel);
+        jTextFieldModel = new JTextField();
+        jTextFieldModel.setLocation(0, 120);
+        jTextFieldModel.setSize(120, 20);
+        add(jTextFieldModel);
+        jLabelPower = new JLabel("Power");
+        jLabelPower.setLocation(0, 160);
+        jLabelPower.setSize(120, 20);
+        add(jLabelPower);
+        jTextFieldPower = new JTextField();
+        jTextFieldPower.setSize(120, 20);
+        jTextFieldPower.setLocation(0, 180);
+        add(jTextFieldPower);
+        jLabelYear = new JLabel("Year");
+        jLabelYear.setLocation(0, 220);
+        jLabelYear.setSize(120, 20);
+        add(jLabelYear);
+        jTextFieldYear = new JTextField();
+        jTextFieldYear.setSize(120, 20);
+        jTextFieldYear.setLocation(0, 260);
+        add(jTextFieldYear);
+        jButtonSave = new JButton("zapisz");
+        jButtonSave.setSize(120, 20);
+        jButtonSave.setLocation(150, 280);
+        add(jButtonSave);
+
+        jButtonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (editedCar!= null) {
+                    editedCar.setYear(Integer.parseInt(jTextFieldYear.getText()));
+                    editedCar.setPower(Double.parseDouble(jTextFieldPower.getText()));
+                    editedCar.setModel(jTextFieldModel.getText());
+                    editedCar.setBrand(jTextFieldBrand.getText());
+                }
+                window.repaint();
+            }
+        });
+
+    }
+
+
+    public void setEditedCar(Car editedCar) {
+        this.editedCar = editedCar;
+        jTextFieldModel.setText(editedCar.getModel());
+        jTextFieldYear.setText(String.valueOf(editedCar.getYear()));
+        jTextFieldPower.setText(String.valueOf(editedCar.getPower()));
+        jTextFieldBrand.setText(editedCar.getBrand());
+    }
+
+    public JTextField getjTextFieldBrand() {
+        return jTextFieldBrand;
+    }
+
+    public void setjTextFieldBrand(JTextField jTextFieldBrand) {
+        this.jTextFieldBrand = jTextFieldBrand;
+    }
+
+    public JTextField getjTextFieldModel() {
+        return jTextFieldModel;
+    }
+
+    public void setjTextFieldModel(JTextField jTextFieldModel) {
+        this.jTextFieldModel = jTextFieldModel;
+    }
+
+    public JTextField getjTextFieldYear() {
+        return jTextFieldYear;
+    }
+
+    public void setjTextFieldYear(JTextField jTextFieldYear) {
+        this.jTextFieldYear = jTextFieldYear;
+    }
+
+    public JTextField getjTextFieldPower() {
+        return jTextFieldPower;
+    }
+
+    public void setjTextFieldPower(JTextField jTextFieldPower) {
+        this.jTextFieldPower = jTextFieldPower;
+    }
+
+    public JButton getjButtonSave() {
+        return jButtonSave;
+    }
+
+    public void setjButtonSave(JButton jButtonSave) {
+        this.jButtonSave = jButtonSave;
+
+    }
+
+
+}
--- carsgui/src/main/java/andrzej/slowinski/cars/CarsManageGui.java
+++ carsgui/src/main/java/andrzej/slowinski/cars/CarsManageGui.java
@@ -15,16 +15,7 @@ public class CarsManageGui extends JFrame {
 
     private CarRepository carRepository;
     private JList<Car> carJList;
-    private JButton editButton;
-    private JLabel jLabelBrand;
-    private JLabel jLabelModel;
-    private JLabel jLabelYear;
-    private JLabel jLabelPower;
-    private JTextField jTextFieldBrand;
-    private JTextField jTextFieldModel;
-    private JTextField jTextFieldYear;
-    private JTextField jTextFieldPower;
-    private JButton jButtonSave;
+
 
     public CarsManageGui() {
         setTitle("cars manager");
@@ -41,97 +32,22 @@ public class CarsManageGui extends JFrame {
         jButton.setSize(120, 20);
         add(jButton);
         carRepository = new CarRepository();
-        editButton = new JButton("edytuj");
-        editButton.setLocation(360, 20);
-        editButton.setSize(120, 20);
-        add(editButton);
-        jLabelBrand = new JLabel("Brand");
-        jLabelBrand.setLocation(360, 80);
-        jLabelBrand.setSize(120, 20);
-        add(jLabelBrand);
-        jTextFieldBrand = new JTextField();
-        jTextFieldBrand.setLocation(360, 100);
-        jTextFieldBrand.setSize(120, 20);
-        add(jTextFieldBrand);
-        jLabelModel = new JLabel("Model");
-        jLabelModel.setSize(120, 20);
-        jLabelModel.setLocation(360, 140);
-        add(jLabelModel);
-        jTextFieldModel = new JTextField();
-        jTextFieldModel.setLocation(360, 160);
-        jTextFieldModel.setSize(120, 20);
-        add(jTextFieldModel);
-        jLabelPower = new JLabel("Power");
-        jLabelPower.setLocation(360, 200);
-        jLabelPower.setSize(120, 20);
-        add(jLabelPower);
-        jTextFieldPower = new JTextField();
-        jTextFieldPower.setSize(120, 20);
-        jTextFieldPower.setLocation(360, 220);
-        add(jTextFieldPower);
-        jLabelYear = new JLabel("Year");
-        jLabelYear.setLocation(360, 260);
-        jLabelYear.setSize(120, 20);
-        add(jLabelYear);
-        jTextFieldYear = new JTextField();
-        jTextFieldYear.setSize(120, 20);
-        jTextFieldYear.setLocation(360, 280);
-        add(jTextFieldYear);
-        jButtonSave = new JButton("zapisz");
-        jButtonSave.setSize(120, 20);
-        jButtonSave.setLocation(360, 350);
-        add(jButtonSave);
+        CarGuiPanel carGuiPanel = new CarGuiPanel(this);
+        carGuiPanel.setLocation(250, 120);
+        carGuiPanel.setSize(300, 300);
+        add(carGuiPanel);
 
-        jButtonSave.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int i = carJList.getSelectedIndex();
-                Car car = carJList.getSelectedValue();
-
-                if (i >= 0) {
-                    String brand = jTextFieldBrand.getText();
-                    String model = jTextFieldModel.getText();
-                    int year = Integer.parseInt(jTextFieldYear.getText());
-                    double power = Double.parseDouble(jTextFieldPower.getText());
-                    car.setYear(year);
-                    car.setPower(power);
-                    car.setModel(model);
-                    car.setBrand(brand);
-                    carJList.repaint();
-                }
-            }
-        });
+;
 
         carJList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                int i = carJList.getSelectedIndex();
-
-                if (i >= 0) {
-                    jTextFieldModel.setText(carJList.getModel().getElementAt(i).getModel());
-                    jTextFieldYear.setText(String.valueOf(carJList.getModel().getElementAt(i).getYear()));
-                    jTextFieldPower.setText(String.valueOf(carJList.getModel().getElementAt(i).getPower()));
-                    jTextFieldBrand.setText(carJList.getModel().getElementAt(i).getBrand());
-
+                Car selectedCar = carJList.getSelectedValue();
+                if (selectedCar != null) {
+                    carGuiPanel.setEditedCar(selectedCar);
                 }
-
             }
         });
-
-//        editButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                int i = carJList.getSelectedIndex();
-//
-//                if (i >= 0) {
-//                    jTextFieldModel.setText(carJList.getModel().getElementAt(i).getModel());
-//                    jTextFieldYear.setText(String.valueOf(carJList.getModel().getElementAt(i).getYear()));
-//                    jTextFieldPower.setText(String.valueOf(carJList.getModel().getElementAt(i).getPower()));
-//                    jTextFieldBrand.setText(carJList.getModel().getElementAt(i).getBrand());
-//
-//                }
-//            }
-//        });
         jButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
